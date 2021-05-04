4254777a58b2bad53d57d7ac494492d3efe98177
Hugo Fitas <hfitas@gmail.com>
2021-05-04 16:56:17 +0100

    added reset file and fixed google font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 import axios from "axios";
+import "./Reset.css";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
