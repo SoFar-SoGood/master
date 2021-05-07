8a2c1e039fb6aeb31758619735b6a4ab7e5cca4e
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-05-07 15:59:46 +0100

    fixed result message.js

--- src/components/ResultMessage/ResultMessage.js
+++ src/components/ResultMessage/ResultMessage.js
@@ -3,24 +3,10 @@ const ResultMessage = (props) => {
   //console.log(props.message);
   return (
     <div>
-      {props.color === "red" && (
-        <div className="result-page-message red">
-          <h1>{props.title}</h1>
-          <h1>{props.message}</h1>
-        </div>
-      )}
-      {props.color === "orange" && (
-        <div className="result-page-message orange">
-          <h1>{props.title}</h1>
-          <h1>{props.message}</h1>
-        </div>
-      )}
-      {props.color === "green" && (
-        <div className="result-page-message green">
-          <h1>{props.title}</h1>
-          <h1>{props.message}</h1>
-        </div>
-      )}
+      <div className={`result-page-message ${props.color}`}>
+        <h1>{props.title}</h1>
+        <h1>{props.message}</h1>
+      </div>
     </div>
   );
 };
